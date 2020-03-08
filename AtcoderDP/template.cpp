90ab2d7be6be7d657c383fcbfa320a245405d42a
Shashi Prakash <sspeedysingh096@gmail.com>
2020-03-08 17:18:38 +0530

    Update Repo

--- AtcoderDP/template.cpp
+++ AtcoderDP/template.cpp
@@ -1,15 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
 
- 
+#define IOS ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define endl "\n"
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) (int)((x).size())
+#define fill(x, y) memset(x, y, sizeof(y))
+#define all(x) (x).begin(), (x).end()
+#define trace(x)  for (auto itr : x)
+#define rep(i, n) for (auto i = 0; i < n; ++i)
+#define repi(i, x, n) for (auto i = x; i <= n; ++i)
+
+typedef vector<int> vi;
+typedef long long  ll;
+
 const int N=1e5+5;
- 
+
 int32_t main()
 {
 	IOS;
 
+
 	return 0;
 }
+
+
+ 
+
+ 
