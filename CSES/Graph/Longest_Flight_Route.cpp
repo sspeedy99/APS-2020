1b8b556269ac14aa72fcab08cc342847b1abaa14
Shashi Prakash <sspeedysingh096@gmail.com>
2021-03-07 21:53:39 +0530

    Update Repo

--- CSES/Graph/Longest_Flight_Route.cpp
+++ CSES/Graph/Longest_Flight_Route.cpp
@@ -1,6 +1,6 @@
 /* Author - sspeedy99
 Editorial - Simple Solution
-The solution is pretty straight forward. We only have to find the shortest path between 1 to n in the given undricted path. Simple BFS is enough, use a vector to print the path */
+Apply Dynamic programming on the topological sorting of the given graph */
 
 
 
@@ -39,39 +39,99 @@ typedef tree<int, null_type,
 #define present(container, element) (container.find(element) != container.end())
 #define notpresent(container, element) (container.find(element) == container.end())
 #define show(container) for(auto it: container) cout<<it<<" "
-#define int long long int
- 
- 
-const int N=1e6+5;
-const int mod = 1e9+7;
-
-vector<bool> vis;
-vector<vector<int>>graph;
-vector<int>dp;
-int n,m;
 
 
-void dfs(int u){
-    vis[u] = true;
-    for(auto it: graph[u]){
-        if(!vis[it])
-            dfs(it);
-        dp[u] = max(dp[u],1+dp[it]);
-    }
+int prev_flight[100000];
+int dist[100000];
+int in_degree[100000];
+vector<int> edge[100000];
+vector<int> backEdge[100000];
+
+int N, M;
+
+//Does a topological sort
+void compute() {
+	queue<int> q;
+	for(int i = 0; i < N; i++) {
+		if(in_degree[i] == 0) {
+			q.push(i);
+		}
+	}
+
+	while(!q.empty()) {
+		int node = q.front();
+		q.pop();
+
+		for(int next : edge[node]) {
+			in_degree[next]--;
+			if(in_degree[next] == 0) q.push(next);
+		}
+
+		//The below block computes the DP
+		int mx = -999999999;
+		int mx_node = -1;
+		for(int prev : backEdge[node]) {
+			if(dist[prev] + 1 > mx) {
+				mx = dist[prev] + 1;
+				mx_node = prev;
+			}
+		}
+
+		dist[node] = mx;
+		if(node == 0) dist[node] = 1;
+		prev_flight[node] = mx_node;
+	}
+
 }
 
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
+	cin >> N >> M;
 
+	for(int i = 0; i < N; i++) {
+		prev_flight[i] = -1;
+		dist[i] = -999999999;
+		in_degree[i] = 0;
+	}
 
-int32_t main()
-{
-    // #ifndef ONLINE_JUDGE
-    // // For getting input from input.txt file
-    // freopen("input.txt", "r", stdin);
- 
-    // // Printing the Output to output.txt file
-    // freopen("output.txt", "w", stdout);
-    IOS;
-    
-    return 0;
+	for(int i = 0; i < M; i++) {
+		int a, b;
+		cin >> a >> b;
+		a--, b--;
+		in_degree[b]++;
+		edge[a].push_back(b);
+
+		backEdge[b].push_back(a);
+	}
+
+	compute();
+
+	stack<int> answer;
+	int temp = N-1;
+
+	bool contains0 = false;
+	if(temp == 0) contains0 = true;
+
+	while(temp != -1 && dist[temp] >= 0) {
+		answer.push(temp);
+		temp = prev_flight[temp];
+		if(temp == 0) contains0 = true;
+	}
+
+	if(contains0) {
+		cout << dist[N-1] << "\n";
+		while(!answer.empty()) {
+			cout << answer.top()+1;
+			answer.pop();
+			if(!answer.empty()) cout << " ";
+		}
+		cout << "\n";
+	} else {
+		cout << "IMPOSSIBLE\n";
+	}
+
+	return 0;
 }
+
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # APS-2020
 Repository of the Course APS-2020
 
-##Important Question - Interview 
+## Important Question - Interview 
 * Clone Graph
 * Clone LinkedList using random pointer.
 * Clone a Binary Search Tree using random pointer.
 * Questions based on modification of Dijikstra's algorithm - Flight Discount CSES
 * Finding K shortest Routes in a graph problem - Modified Dijikstra
+* Classical Problems of DP - LPS
+* Longest Flight Route - DP on Graphs (Topo sort with DP)
