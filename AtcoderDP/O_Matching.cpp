fea999ad02c698d3fd73ef9f589ff9f259745c71
Shashi Prakash <sspeedysingh096@gmail.com>
2021-05-17 20:29:58 +0530

    Update Status

--- AtcoderDP/O_Matching.cpp
+++ AtcoderDP/O_Matching.cpp
@@ -37,6 +37,12 @@ const int N=1e6+5;
 const int mod = 1e9+7;
 const ll inf = 1e18;
 
+void add_self(int& a, int b) {
+    a += b;
+    if(a >= mod) {
+        a -= mod;
+    }
+}
 
  
 int main()
@@ -48,5 +54,26 @@ int main()
     // // Printing the Output to output.txt file
     // freopen("output.txt", "w", stdout);
     IOS;
+    int n;
+    cin>>n;
+    vector<vector<int>> can(n, vector<int>(n));
+    for(int i =0; i<n; i++){
+        for(int j =0; j<n; j++){
+            cin>>can[i][j];
+        }
+    }
+    vector<int>dp(1<<n);
+    dp[0] = 1;
+    for(int mask = 0; mask<(1<<n)-1; mask++){
+        int a = __builtin_popcount(mask);
+        for(int b = 0; b<n; b++){
+            if(can[a][b] && !(mask&(1<<b))){
+                int m2 = mask^(1<<b);
+                add_self(dp[m2],dp[mask]);
+            }
+        }
+    }
+    cout<<dp[(1<<n)-1];
+
     return 0;
 }
