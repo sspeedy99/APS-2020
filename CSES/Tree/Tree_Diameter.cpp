452b62fc41f75466df6d8e64aba7e2d5912194bd
Shashi Prakash <sspeedysingh096@gmail.com>
2021-05-25 21:52:12 +0530

    Update Repo

--- CSES/Tree/Tree_Diameter.cpp
+++ CSES/Tree/Tree_Diameter.cpp
@@ -1,91 +1,54 @@
-/* Author - sspeedy99
-Editorial: Important question - find the diameter in the tree
-*/
-
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp>
-#include <functional> // for less
-#include <iostream>
-using namespace __gnu_pbds;
-using namespace std;
-using vi = vector<int>;
-using vii = vector<long long>;
- 
- 
-// Policy based data structure - order statistics tree with node update
-typedef tree<int, null_type,
-             less_equal<int>, rb_tree_tag,
-             tree_order_statistics_node_update>
-    Ordered_set;
- 
- 
-#define IOS ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define endl "\n"
-#define mp make_pair
+#include<bits/stdc++.h>
+#define REP(i,n) for (int i = 1; i <= n; i++)
+#define mod 1000000007
 #define pb push_back
-#define ll long long
-#define fi first
-#define se second
-#define sz(x) (int)(x).size()
-#define fill(x, y) memset(x, y, sizeof(x))
-#define all(x) (x).begin(), (x).end()
-#define trace(itr, x)  for (auto itr : x)
-#define rep(i, n) for (int i = 0; i < n; ++i)
-#define repi(i, x, n) for (auto i = x; i <= n; ++i)
-#define present(container, element) (container.find(element) != container.end())
-#define notpresent(container, element) (container.find(element) == container.end())
-#define show(container) for(auto it: container) cout<<it<<" "
-#define int long long
+#define ff first
+#define ss second
+#define ii pair<int,int>
+#define vi vector<int>
+#define vii vector<ii>
+#define lli long long int
+#define INF 1000000000
+#define endl '\n'
+const double PI = 3.141592653589793238460;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+using namespace std;
  
+vi ar[200001];
+int f[200001];
+int g[200001];
+int d;
  
-const int N=1e6+5;
-const int mod = 1e9+7;
-const ll mex = 2e5 + 5;
-
-vector<vector<int>>children; // to store the children of every node. children[parent_node] -> child_node1, child_node2
-vector<int>F, G; // subtree[node] -> size of subtree whose root is node, depth[node] -> depth of node 
-int diameter;
-
-void solve(int V, int parent){
-    vector<int>fValues;
-    for(auto v: children[V]){
-        if(v==parent) continue;
-        solve(v,V);
-        fValues.pb(F[v]);
-    }
-
-    sort(all(fValues));
-    F[V] = 1;
-    if(not fValues.empty()) F[V] += fValues.back();
-    if(fValues.size() >= 2)
-        G[V] = 2 + fValues.back() + fValues[fValues.size() - 2];
-    
-    diameter = max(diameter,max(F[V], G[V]));
-}
-
-
-int32_t main()
+void dfs(int node ,int par)
 {
-    // #ifndef ONLINE_JUDGE
-    // // For getting input from input.txt file
-    // freopen("input.txt", "r", stdin);
+	vi childList;
+	
+	for(int child : ar[node])
+	if(child != par)
+	{
+		dfs(child , node);
+		f[node] = max(f[node] , f[child] + 1);
+		childList.pb(f[child]);
+	}
+	
+	d = max(d , f[node]);
+	
+	sort(childList.begin() , childList.end());
+	if(childList.size() >= 2)
+	{
+		g[node] = 2 + childList[childList.size()-1] + childList[childList.size()-2];
+		d = max(d , g[node]);
+	}
+}
  
-    // // Printing the Output to output.txt file
-    // freopen("output.txt", "w", stdout);
-    IOS;
-    int n;
-    cin>>n;
-    children.resize(n+1);
-    F.resize(n+1);
-    G.resize(n+1);
-    for(int i=0; i<n-1; i++){
-        int u,v;
-        cin>>u>>v;
-        children[u].pb(v);
-    }
-    solve(1,0);
-    cout<<diameter<<endl;
-
-    return 0;
+int main()
+{
+	int n , a , b;
+	cin>>n;
+	REP(i , n-1) cin>>a>>b , ar[a].pb(b) , ar[b].pb(a);
+	
+	dfs(1 , -1);
+	
+	cout<<d;
 }
